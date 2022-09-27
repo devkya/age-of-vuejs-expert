f3f5c51dee0b3adbb40450d3c248565e76e76e97
devKya <kihaok@gmail.com>
2022-09-27 10:10:43 +0900

    Create Login API

--- vue-til/src/components/LoginForm.vue
+++ vue-til/src/components/LoginForm.vue
@@ -1,30 +1,49 @@
 <template>
 	<div>
-		<form>
+		<form @submit.prevent="submitForm">
 			<div>
 				<label for="username">id :</label>
-				<input id="username" type="text" />
+				<input id="username" type="text" v-model="username" />
 			</div>
 			<div>
 				<label for="password">pw :</label>
-				<input id="password " type="text" />
+				<input id="password " type="text" v-model="password" />
 			</div>
 			<button type="submit">로그인</button>
 		</form>
+		<p>{{ logMessage }}</p>
 	</div>
 </template>
 <script>
+import { loginUser } from '@/api/index';
 export default {
 	components: {},
 	data() {
 		return {
-			sampleData: '',
+			username: '',
+			password: '',
+			logMessage: '',
 		};
 	},
 	setup() {},
 	created() {},
 	mounted() {},
 	unmounted() {},
-	methods: {},
+	methods: {
+		async submitForm() {
+			const userData = {
+				username: this.username,
+				password: this.password,
+			};
+			const { data } = await loginUser(userData);
+			this.logMessage = `${data.user.username} 님 환영합니다`;
+			console.log(data);
+			this.initForm();
+		},
+		initForm() {
+			this.username = '';
+			this.password = '';
+		},
+	},
 };
 </script>
